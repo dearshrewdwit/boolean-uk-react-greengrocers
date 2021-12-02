482d8426201c528baf437db65486fe53967256e3
bravint <bravint@gmail.com>
2021-12-02 16:55:47 +0000

    add data.js

---
+++ src/data/items.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: "001-beetroot",
+        name: "beetroot",
+        price: 1.80,
+        image: "assets/icons/001-beetroot.svg"
+    },
+    {
+        id: "002-carrot",
+        name: "carrot",
+        price: 0.40,
+        image: "assets/icons/002-carrot.svg"
+    },
+    {
+        is: "003-apple",
+        name: "apple",
+        price: 0.62,
+        image: "assets/icons/003-apple.svg"
+    },
+    {
+        is: "004-apricot",
+        name: "apricot",
+        price: 1.75,
+        image: "assets/icons/004-apricot.svg"
+    },
+    {
+        is: "005-avocado",
+        name: "avocado",
+        price: 0.75,
+        image: "assets/icons/005-avocado.svg"
+    },
+    {
+        is: "006-bananas",
+        name: "bananas",
+        price: 0.64,
+        image: "assets/icons/006-bananas.svg"
+    },
+    {
+        is: "007-bell-pepper",
+        name: "bell-pepper",
+        price: 0.45,
+        image: "assets/icons/007-bell-pepper.svg"
+    },
+    {
+        is: "008-berry",
+        name: "berry",
+        price: 2.00,
+        image: "assets/icons/008-berry.svg"
+    },
+    {
+        is: "009-blueberry",
+        name: "blueberry",
+        price: 2.00,
+        image: "assets/icons/009-blueberry.svg"
+    },
+    {
+        is: "010-eggplant",
+        name: "eggplant",
+        price: 0.69,
+        image: "assets/icons/010-eggplant.svg"
+    }
+]
