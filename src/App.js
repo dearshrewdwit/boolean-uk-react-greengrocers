b7dad262f9887b2daf16e93dec13bf6c730af3d5
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-03-02 11:34:56 +0000

    standard criteria

--- src/App.js
+++ src/App.js
@@ -1,37 +1,69 @@
 import './styles/reset.css'
 import './styles/index.css'
-
+import { useState } from 'react'
 import initialStoreItems from './store-items'
 
-/*
-Here's what a store item should look like
-{
-  id: '001-beetroot',
-  name: 'beetroot',
-  price: 0.35
-}
-
-What should a cart item look like? 🤔
-*/
-
-console.log(initialStoreItems)
-
-export default function App() {
+export default function App () {
   // Setup state here...
+  const [store, setStore] = useState(initialStoreItems)
+  const [cart, setCart] = useState([])
 
+  function addToCart (item) {
+    checkQuantity(item.id) ? IncQuantity(item) : setCart([...cart, { ...item }])
+  }
+  function removeFromCart (item) {
+    item.quantity === 1 ? setCart(x => x.filter(c => c.id !== item.id)) : DecQuantity(item)
+  }
+  function DecQuantity (item) {
+    setCart(x => x.map(c => c.id === item.id ? { ...c, quantity: c.quantity - 1 } : c))
+  }
+  function IncQuantity (item) {
+    setCart(x => x.map(c => c.id === item.id ? { ...c, quantity: c.quantity + 1 } : c))
+  }
+  function checkQuantity (id) {
+    return cart.some(x => x.id === id)
+  }
+  const total = cart.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0)
+
+  const displayCart = cart.map(x => {
+    return (
+      <li key={ x.id } >
+        <img
+          class="cart--item-icon"
+          src={ "/assets/icons/" + x.id + '.svg' }
+          alt={ x.name }
+        />
+        <p>{ x.name }</p>
+        <button onClick={ () => removeFromCart(x) } class="quantity-btn remove-btn center">-</button>
+        <span class="quantity-text center">{ x.quantity }</span>
+        <button onClick={ () => addToCart(x) } class="quantity-btn add-btn center">+</button>
+      </li>
+    )
+  })
+
+  const displayedStore = store.map(x => {
+    return (
+      <li key={ x.id } >
+        <div class="store--item-icon">
+          <img src={ "/assets/icons/" + x.id + '.svg' } alt={ x.name } />
+        </div>
+        <button onClick={ () => addToCart(x) }>Add to cart</button>
+      </li>
+    )
+  })
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          { displayedStore }
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+            { displayCart }
           </ul>
         </div>
         <div className="total-section">
@@ -39,7 +71,7 @@ export default function App() {
             <h3>Total</h3>
           </div>
           <div>
-            <span className="total-number">£0.00</span>
+            <span className="total-number">{ '£' + total.toFixed(2) }</span>
           </div>
         </div>
       </main>
--- src/store-items.js
+++ src/store-items.js
@@ -1,54 +1,74 @@
 
-const storeItems =  [
+const storeItems = [
   {
     id: "001-beetroot",
     name: "beetroot",
-    price: 0.35
+    price: 1.45,
+    quantity: 1,
+    isFruite: false
   },
   {
     id: "002-carrot",
     name: "carrot",
-    price: 0.35
+    price: 5.55,
+    quantity: 1,
+    isFruite: false
   },
   {
     id: "003-apple",
     name: "apple",
-    price: 0.35
+    price: 0.95,
+    quantity: 1,
+    isFruite: true
   },
   {
     id: "004-apricot",
     name: "apricot",
-    price: 0.35
+    price: 0.75,
+    quantity: 1,
+    isFruite: false
   },
   {
     id: "005-avocado",
     name: "avocado",
-    price: 0.35
+    price: 0.85,
+    quantity: 1,
+    isFruite: true
   },
   {
     id: "006-bananas",
     name: "bananas",
-    price: 0.35
+    price: 0.99,
+    quantity: 1,
+    isFruite: true
   },
   {
     id: "007-bell-pepper",
     name: "bell pepper",
-    price: 0.35
+    price: 1.35,
+    quantity: 1,
+    isFruite: false
   },
   {
     id: "008-berry",
     name: "berry",
-    price: 0.35
+    price: 2.35,
+    quantity: 1,
+    isFruite: true
   },
   {
     id: "009-blueberry",
     name: "blueberry",
-    price: 0.35
+    price: 4.35,
+    quantity: 1,
+    isFruite: true
   },
   {
     id: "010-eggplant",
     name: "eggplant",
-    price: 0.35
+    price: 4.35,
+    quantity: 1,
+    isFruite: false
   }
 ]
 
