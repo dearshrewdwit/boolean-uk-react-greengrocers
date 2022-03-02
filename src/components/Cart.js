34922634db4463133d063dafd04f3b3f36541f7c
Usama Lodhi <lodhius@gmail.com>
2022-03-02 11:17:46 +0000

    proudcts render on screen

---
+++ src/components/Cart.js
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+
+const Cart = () => {
+  return (
+    <main id="cart">
+      <h2>Your Cart</h2>
+      <div class="cart--item-list-container">
+        <ul class="item-list cart--item-list">
+          {/* Wrtite some code here... */}
+        </ul>
+      </div>
+      <div class="total-section">
+        <div>
+          <h3>Total</h3>
+        </div>
+        <div>
+          <span class="total-number">£0.00</span>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Cart
---
+++ src/components/Store.js
@@ -0,0 +1,30 @@
+// import { useState } from 'react'
+import initialStoreItems from '../data/StoreItems'
+
+const Store = () => {
+  // const [storeItems, setStoreItems] = useState(initialStoreItems)
+  // const [cartItems, setCartItems] = useState([])
+
+  return (
+    <header id="store">
+      <h1>Greengrocers</h1>
+      <ul className="item-list store--item-list">
+        {initialStoreItems.map(item => (
+          <li key={item.id}>
+            <section>
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+            </section>
+            <div className="store--item-icon">
+              <img src={`../assets/icons/${item.id}.svg`} alt={item.name} />
+            </div>
+            <button className="btn">Add to cart</button>
+          </li>
+        ))}
+      </ul>
+    </header>
+  )
+}
+
+export default Store
+// onClick={() => addToCartHandler()}
