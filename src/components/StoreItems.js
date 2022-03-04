a07e8330da827dd5b43dd7dafd5df986e5d66601
willyc <willcowendrums@gmail.com>
2022-03-04 09:39:24 +0000

    final

--- src/components/StoreItems.js
+++ src/components/StoreItems.js
@@ -3,13 +3,8 @@
 function StoreItems(props) {
 
   const addItemToCart = target => {
-    //check if the item exists in the cart already - will return undefined if not.
     const itemExists = props.cart.find(item => item.id === target.id)
-  
-    //if item exists update that items quantity key by 1
     if (itemExists) props.setCart(cart => cart.map(item => item.id === target.id ? {...item, quantity: item.quantity+1} : item))
-    
-    //otherwise add the item and add a new key of quantity, with the value of 1.
     else props.setCart(cart => [...cart, {...target, "quantity": 1}])
   }
 
