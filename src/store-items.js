e98ddd61ecfd290525557e1b61fefee80e541edb
Nico Picchio <nicolapicchio@gmail.com>
2022-03-04 16:03:42 +0000

    items prices modified to implement extension 1 part II

--- src/store-items.js
+++ src/store-items.js
@@ -2,61 +2,61 @@ const storeItems = [
   {
     id: '001-beetroot',
     name: 'beetroot',
-    price: 0.35,
+    price: 0.47,
     type: 'veg'
   },
   {
     id: '002-carrot',
     name: 'carrot',
-    price: 0.35,
+    price: 0.22,
     type: 'veg'
   },
   {
     id: '003-apple',
     name: 'apple',
-    price: 0.35,
+    price: 0.89,
     type: 'fruit'
   },
   {
     id: '004-apricot',
     name: 'apricot',
-    price: 0.35,
+    price: 1.04,
     type: 'fruit'
   },
   {
     id: '005-avocado',
     name: 'avocado',
-    price: 0.35,
+    price: 1.44,
     type: 'fruit'
   },
   {
     id: '006-bananas',
     name: 'bananas',
-    price: 0.35,
+    price: 0.98,
     type: 'fruit'
   },
   {
     id: '007-bell-pepper',
     name: 'bell pepper',
-    price: 0.35,
+    price: 0.65,
     type: 'veg'
   },
   {
     id: '008-berry',
     name: 'berry',
-    price: 0.35,
+    price: 1.35,
     type: 'fruit'
   },
   {
     id: '009-blueberry',
     name: 'blueberry',
-    price: 0.35,
+    price: 1.89,
     type: 'fruit'
   },
   {
     id: '010-eggplant',
     name: 'eggplant',
-    price: 0.35,
+    price: 0.68,
     type: 'veg'
   }
 ]
