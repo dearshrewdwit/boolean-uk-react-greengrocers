e5362f2fd6eb3862b688f041dd6670fdf8237b82
MattVwaves <jamesmattholt@gmail.com>
2022-11-10 15:41:12 +0000

    all basics met within app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,114 @@
+
 import './styles/reset.css'
 import './styles/index.css'
+// import './public/assets'
+
+import {useState} from'react'
 
 import initialStoreItems from './store-items'
 
-/*
-Here's what a store item should look like
-{
-  id: '001-beetroot',
-  name: 'beetroot',
-  price: 0.35
-}
-
-What should a cart item look like? 🤔
-*/
-
-console.log(initialStoreItems)
-
 export default function App() {
-  // Setup state here...
+
+  const [storeItems, setStoreItems] = useState(initialStoreItems)
+  const [cartItems, setCartItems] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const addCartItem = (item)=>{
+    const duplicates = cartItems.find(cartItem=> cartItem.name === item.name)
+    if (duplicates === undefined){
+    item.quantity = 1
+    const newCart = [...cartItems, item]
+    setCartItems(newCart)
+    }
+    else {
+      const ammendedCart = cartItems.map((cartItem)=>{
+        if (cartItem.name === item.name){
+          const itemCopy = {...item, quantity: cartItem.quantity +1}
+          return itemCopy
+        }
+        else {
+          return cartItem
+        }
+        
+      })
+      setCartItems(ammendedCart)
+    }
+    setTotal(total + item.price)
+  }
+
+  const handlePlus = (cartItem)=>{
+    const ammendedCart = cartItems.map((storedItem)=>{
+      if (storedItem === cartItem){
+        const cartItemCopy = {...cartItem, quantity: cartItem.quantity + 1}
+        return cartItemCopy
+      }
+      else {
+        return storedItem
+      }
+    })
+    setCartItems(ammendedCart)
+    setTotal(total + cartItem.price)
+  }
+  const handleMinus = (cartItem)=>{
+    let ammendedCart = []
+    if (cartItem.quantity > 1){
+    ammendedCart = cartItems.map((storedItem)=>{
+      if (storedItem === cartItem){
+        const cartItemCopy = {...cartItem, quantity: cartItem.quantity - 1}
+        return cartItemCopy
+      }
+      else {
+        return storedItem
+      }
+    })
+      }
+      else {
+        ammendedCart = cartItems.filter((storedItem)=> storedItem !== cartItem)
+      }
+      setCartItems(ammendedCart)
+      setTotal(total - cartItem.price)
+  }
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          {
+            storeItems.map((item)=>{
+              return (
+                <li key={item.id}>
+                  <div class="store--item-icon">
+                    <img src={"/assets/icons/" + item.id + ".svg"} alt={item.name} />
+                  </div>
+                  <button onClick={()=> addCartItem(item) }>Add to cart</button>
+                </li>
+              )
+            })
+          }
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+            {
+              cartItems.map((cartItem)=>{
+                return (
+                  <li key={cartItem.id}>
+                    <img
+                      class="cart--item-icon"
+                      src={"/assets/icons/" + cartItem.id + ".svg"}
+                      alt={cartItem.name}
+                    />
+                    <p>{cartItem.name}</p>
+                    <button onClick={()=> handleMinus(cartItem) } class="quantity-btn remove-btn center">-</button>
+                    <span class="quantity-text center">{cartItem.quantity}</span>
+                    <button onClick={()=> handlePlus(cartItem) } class="quantity-btn add-btn center">+</button>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
         <div className="total-section">
@@ -39,7 +116,7 @@ export default function App() {
             <h3>Total</h3>
           </div>
           <div>
-            <span className="total-number">£0.00</span>
+            <span className="total-number">{'£' + (Math.round(total * 100) / 100).toFixed(2)}</span>
           </div>
         </div>
       </main>
