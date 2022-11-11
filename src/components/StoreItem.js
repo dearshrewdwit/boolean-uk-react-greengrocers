21a964cec26e0b9a267960dde1e7f0e2c1b7bd9f
MattVwaves <jamesmattholt@gmail.com>
2022-11-11 13:03:34 +0000

    item details toggle added/conditinoal rendering

--- src/components/StoreItem.js
+++ src/components/StoreItem.js
@@ -1,13 +1,24 @@
+import {useState} from'react'
 
 function StoreItem ({item, addCartItem}) {
+
+  const [details, setDetails] = useState(false)
+  console.log(details)
     
     return (
+      <>
         <li key={item.id}>
           <div class="store--item-icon">
-            <img src={"/assets/icons/" + item.id + ".svg"} alt={item.name} />
+            <img onClick={()=> setDetails(!details)}src={"/assets/icons/" + item.id + ".svg"} alt={item.name} />
           </div>
           <button onClick={()=> addCartItem(item) }>Add to cart</button>
+          {details === true &&
+        <ul className="details">
+          <li>{item.name}</li>
+          <li>{'£' + item.price}</li>
+          </ul>}
         </li>
+        </>
       )
 }
 export default StoreItem
