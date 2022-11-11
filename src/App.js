739904e031240764ba35efc909394bf8435e4b33
Fatima <fxtimaahmed14@gmail.com>
2022-11-11 13:44:10 +0000

    lots of beets in the cart + trying to get cart to work

--- src/App.js
+++ src/App.js
@@ -12,10 +12,14 @@ Here's what a store item should look like
 {
   id: '001-beetroot',
   name: 'beetroot',
-  price: 0.35
+  price: 0.35,
+  (need to add the quantity prop)
 }
 
 What should a cart item look like? 🤔
+
+It needs to have a qunatity prop just like how we added one in vanilla
+but how do I do that !!! :(
 */
 
 console.log(initialStoreItems)
--- src/Cart.js
+++ src/Cart.js
@@ -1,11 +1,34 @@
+import { useState } from "react"
+import initialStoreItems from './store-items'
+
 function Cart (props) {
-    // {} = props
+  // const {} = props
+  const [ cartItems, setCartItem] = useState(initialStoreItems)
+  // const [total , setTotal] = useState(0)
+
+  // const addToCart = ()
+
     return (
         <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+
+          {cartItems.map(cartItem => {
+            return (
+              <li>
+                <img
+                  class="cart--item-icon"
+                  src="assets/icons/001-beetroot.svg"
+                  alt="beetroot"
+                />
+                <p>beetroot</p>
+                <button class="quantity-btn remove-btn center">-</button>
+                <span class="quantity-text center">1</span>
+                <button class="quantity-btn add-btn center">+</button>
+              </li>
+            )
+          })}
           </ul>
         </div>
         <div className="total-section">
@@ -28,4 +51,6 @@ export default Cart
 
 // Import state @ the top
 // Use a state called cart Itmes and setCartItem
+// Create addToCart function here that processes store click event 
 // need a state for total ?? => total + setTotal?
+// Work out how to do total in here than move to another file and import it here
