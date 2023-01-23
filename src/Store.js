8294b31e7a650135e4553578a8b562868a2c9413
benji <benjipictor@gmail.com>
2023-01-23 15:56:27 +0000

    sorting added to store

--- src/Store.js
+++ src/Store.js
@@ -8,23 +8,34 @@ import FilterByType from "./filter-by-type";
 
 function Store(props) {
     const [storeItems, setStoreItems] = useState(initialStoreItems)
-    const [filter, setFilter] = useState([])
+    const [filter, setFilter] = useState('')
+    const [sort, setSort] = useState('')
     const fruit = storeItems.filter(item => item.type === "fruit")
     const veg = storeItems.filter(item => item.type === "veg")
+    
         
     const filterItems = () => {
         let eValue = event.target.value;
         setFilter(eValue)
         console.log(eValue)
     }
-
+    const sortItems = () => {
+        let eValue = event.target.value;
+        setSort(eValue)
+        console.log(eValue)
+        console.log(filterdList)
+    }
     let filterdList = storeItems
+    
     if(filter === 'all'){filterdList = storeItems}
     if(filter === 'Fruit'){filterdList = fruit}
-    if(filterdList === 'veg'){filterdList = veg}
+    if(filter === 'Veg'){filterdList = veg}
+    if(sort === 'price-accending'){filterdList.sort((a,b) => a.price - b.price)}
+    if(sort === 'price-decending'){filterdList.sort((a,b) => a.price - b.price).reverse()}
+    console.log("this is filterdList", filterdList )
     return (
         <ul className="item-list store--item-list">
-            <FilterByType filter={filterItems} veg={veg} fruit={fruit} setStoreItems={setStoreItems} />
+            <FilterByType sort={sortItems} filter={filterItems} veg={veg} fruit={fruit} setStoreItems={setStoreItems} />
             {filterdList.map((item, index) => (
                 <Item storeItem={item} key={index} addToCart={props.addToCart} />
 
--- src/filter-by-type.js
+++ src/filter-by-type.js
@@ -3,12 +3,16 @@ function FilterByType(props) {
     
     return (
         <div>
-            
-            <label htmlFor="sort-by-type">Select fruit or Veg</label>
+            <label htmlFor="sort-by price">Sort by Price</label>
 
+            <select name="sort-by-price" id="price" onChange={() => props.sort()}>
+                <option value=''></option>
+                <option value="price-decending">High - Low</option>
+                <option value="price-accending">Low - High</option>
+            </select>
+            <label htmlFor="sort-by-type">Select fruit or Veg</label>
             <select name="sort-by-type" id="fruitORVeg" onChange={(event) => props.filter()}>
-                <option defaultValue='all' >Choose</option>
-                <option value="all">All</option>
+                <option value="all" >All</option>
                 <option value="Fruit">Fruit</option>
                 <option value="Veg">Vegatables</option>
             </select>
