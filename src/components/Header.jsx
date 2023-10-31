33f2599d1b41533e4a646493fdf97e95346dfd01
Kye <NovusAa12@proton.me>
2023-10-31 21:33:06 +0000

    add filter function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,39 @@
-const Header = ({ store, cart, setCart}) => {
+import { useState } from "react";
+
+const Header = ({ store, cart, setCart }) => {
+    const [filtered, setFiltered] = useState(store);
+
     const addToCart = (item) => {
         item.quantity++;
 
         const duplicate = cart.some(obj => { return obj.id == item.id });
 
-        duplicate ? setCart([...cart]) : (setCart([...cart, item]))
+        duplicate ? setCart([...cart]) : (setCart([...cart, item]));
+    };
+
+    const filter = () => {
+      const filter = document.querySelector("#filter");
+      const option = filter.value;
+
+      const filterByFruit = store.filter(item => item.type === "fruit");
+      const filterByVeg = store.filter(item => item.type === "veg");
+
+      option === "fruit" ? setFiltered(filterByFruit)
+      : option === "veg" ? setFiltered(filterByVeg)
+      : setFiltered(store);
     };
 
     return (
     <>
     <header id="store">
     <h1>Greengrocers</h1>
+    <select name="filter" id="filter" onChange={() => filter()}>
+      <option value="filter">All</option>
+      <option value="fruit">Fruits</option>
+      <option value="veg">Vegetables</option>
+    </select>
     <ul className="item-list store--item-list">
-      {store.map(item => 
+      {filtered.map(item => 
         <li key={item.id}>
             <div className="store--item-icon">
                 <img src={`/assets/icons/${item.id}.svg`} alt={item.name} />
--- src/store-items.js
+++ src/store-items.js
@@ -4,61 +4,71 @@ const storeItems =  [
     id: "001-beetroot",
     name: "beetroot",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "veg"
   },
   {
     id: "002-carrot",
     name: "carrot",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "veg"
   },
   {
     id: "003-apple",
     name: "apple",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   },
   {
     id: "004-apricot",
     name: "apricot",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   },
   {
     id: "005-avocado",
     name: "avocado",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   },
   {
     id: "006-bananas",
     name: "bananas",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   },
   {
     id: "007-bell-pepper",
     name: "bell pepper",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "veg"
   },
   {
     id: "008-berry",
     name: "berry",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   },
   {
     id: "009-blueberry",
     name: "blueberry",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   },
   {
     id: "010-eggplant",
     name: "eggplant",
     price: 0.35,
-    quantity: 0
+    quantity: 0,
+    type: "fruit"
   }
 ]
 
