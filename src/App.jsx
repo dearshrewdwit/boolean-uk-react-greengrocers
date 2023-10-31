aa908bb45859b0203af146d1925fc94441c6fe2e
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-10-31 18:59:21 +0000

    cart component worked on

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import "./styles/reset.css";
 import "./styles/index.css";
 
 
-//import Header from "./Header";
+import Header from "./Header";
 import Items from "./Items";
-// import Cart from "./Cart";
+import Cart from "./Cart";
 
 
 import initialStoreItems from "./store-items";
@@ -36,7 +36,8 @@ components
 */
 
 export default function App() {
- const [storeItems, setStoreItems] = useState(initialStoreItems)
+  const [storeItems] = useState(initialStoreItems)
+  const [cartItems, setCartItems] = useState([])
 
   return (
     <>
@@ -44,9 +45,9 @@ export default function App() {
         <h1>Greengrocers</h1>
 
         <ul className="item-list store--item-list">
-        {initialStoreItems.map(item => (
-    <Items key={item.id} item={item} />
-  ))}
+          {initialStoreItems.map(item => (
+            <Items key={item.id} item={item} />
+          ))}
         </ul>
 
 
@@ -55,7 +56,9 @@ export default function App() {
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Write some code here... */}
+            {cartItems.map(cartItem => (
+              <Cart />
+            ))}
           </ul>
         </div>
         <div className="total-section">
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,42 @@
+import React, { useState } from "react";
 
+function CartItem({ item }) {
+  const [quantity, setQuantity] = useState(item.quantity);
 
+  const handleQuantityChange = (change) => {
+    const newQuantity = Math.max(quantity + change, 0); // Ensure quantity doesn't go below 0
+    if (newQuantity === 0) {
+      // If the quantity becomes 0, remove the item from the cart
+      // You can call a function here to remove the item from the cart state in your parent component
+    } else {
+      // If the quantity is greater than 0, update the quantity in the cart
+      setQuantity(newQuantity);
+    }
+  };
 
+  return (
+    <li>
+      <img
+        className="cart--item-icon"
+        src={`assets/icons/${item.id}.svg`}
+        alt={item.name}
+      />
+      <p>{item.name}</p>
+      <button
+        className="quantity-btn remove-btn center"
+        onClick={() => handleQuantityChange(-1)}
+      >
+        -
+      </button>
+      <span className="quantity-text center">{quantity}</span>
+      <button
+        className="quantity-btn add-btn center"
+        onClick={() => handleQuantityChange(1)}
+      >
+        +
+      </button>
+    </li>
+  );
+}
 
-/*
-return(
-<li>
-  <img
-    class="cart--item-icon"
-    src="assets/icons/001-beetroot.svg"
-    alt="beetroot"
-  />
-  <p>beetroot</p>
-  <button class="quantity-btn remove-btn center">-</button>
-  <span class="quantity-text center">1</span>
-  <button class="quantity-btn add-btn center">+</button>
-</li>
-)
-*/
+export default CartItem;
