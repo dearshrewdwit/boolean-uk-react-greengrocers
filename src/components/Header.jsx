2ec313500a1edf1efd740b071a05663f443a343a
aayushlama4008 <aayushlama4008@gmail.com>
2023-10-31 17:57:17 +0100

    added the items in the store

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,16 @@ function Header({storeItem}){
         <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          
+          {storeItem.map((item)=>{
+            return(
+                <li key={item.id}>
+                    <div className="store--item-icon">
+                        <img src={`/assets/icons/${item.id}.svg`} alt={`${item.name}`} />
+                    </div>
+                    <button>Add to cart</button>
+                </li>
+            )
+          })}
         </ul>
       </header>
     )
