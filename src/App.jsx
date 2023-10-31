6de6aec54e00721c1e33559ee57a37be5974c48e
Jack Miller <jdmiller1991@outlook.com>
2023-10-31 11:57:28 +0000

    First part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/reset.css'
 import './styles/index.css'
 
+import { useState } from 'react'
 import initialStoreItems from './store-items'
 
 /*
@@ -17,14 +18,22 @@ import initialStoreItems from './store-items'
 console.log(initialStoreItems)
 
 export default function App() {
-  // Setup state here...
+  const [items, setItems] = useState(initialStoreItems)
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Write some code here... */}
+          {items.map((item) => (
+            <li key={item.id}>
+              <div className="store--item-icon">
+                <img src={`/assets/icons/${item.id}.svg`} alt={item.name} />
+              </div>
+              <button>Add to cart</button>
+              <p>{item.name.toUpperCase()}</p>
+              <p>${item.price.toFixed(2)}</p>
+            </li>))}
         </ul>
       </header>
       <main id="cart">
