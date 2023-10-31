6994f41579ed9665a6ea5d343ce5615ea381166e
aayushlama4008 <aayushlama4008@gmail.com>
2023-11-01 00:37:50 +0100

    maintained the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,28 @@
-import './styles/reset.css'
-import './styles/index.css'
+import "./styles/reset.css";
+import "./styles/index.css";
 
-import initialStoreItems from './store-items'
-import { useState } from 'react'
+import initialStoreItems from "./store-items";
+import { useState } from "react";
 
 //importing the jsx
-import Header from './components/Header'
-import Main from './components/Main'
-
-/*
- Here's what a store item should look like
- {
- id: '001-beetroot',
- name: 'beetroot',
- price: 0.35
- }
-
- What should a cart item look like? 🤔
- */
-
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 export default function App() {
   // Setup state here...
-  const [storeItem,setStoreItem] = useState(initialStoreItems)
-  const [cartItem , setCartItem] = useState([])
+  const [storeItem, setStoreItem] = useState(initialStoreItems);
+  const [cartItem, setCartItem] = useState([]);
 
   return (
     <>
-      <Header storeItem={storeItem} cartItem={cartItem}
-      setCartItem={setCartItem}/>
-      <Main cartItem={cartItem} setCartItem={setCartItem}/>
-      <div>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/icongeek26"
-          title="Icongeek26"
-        >
-          Icongeek26
-        </a>
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
+      <Header
+        storeItem={storeItem}
+        cartItem={cartItem}
+        setCartItem={setCartItem}
+      />
+      <Main cartItem={cartItem} setCartItem={setCartItem} />
+      <Footer />
     </>
-  )
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,54 +1,32 @@
+import Inside from "./Inside";
 
+function Main({ cartItem, setCartItem }) {
+  console.log(cartItem);
 
-function Main({cartItem,setCartItem}){
-  console.log(cartItem)
-    function addQuantity(inputItem){
-      inputItem.quantity++;
-      setCartItem([...cartItem])
-    }
+  function total() {
+    const amount = cartItem.reduce((sum, item) => {
+      return sum + item.price * item.quantity;
+    }, 0);
+    return amount.toFixed(2);
+  }
 
-    function minusQuantity(inputItem){
-      inputItem.quantity--;
-      if(inputItem.quantity === 0){
-        cartItem = cartItem.filter((product)=>product.id !== inputItem.id)
-      }  
-      setCartItem([...cartItem])
-    }
-
-    function total(){
-       const amount =cartItem.reduce((sum,item)=> {
-         return sum +(item.price *item.quantity)},0)
-      return  amount.toFixed(2)
-    }
-
-    return(
-        <main id="cart">
-        <h2>Your Cart</h2>
-        <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
-            {cartItem.map(newItem =>{
-              return(
-                <li key={newItem.id}>
-                  <img src={`assets/icons/${newItem.id}.svg`} alt={`${newItem.name}`} 
-                  className="cart--item-icon" />
-                  <p>{newItem.name}</p>
-                  <button className="quantity-btn remove-btn center" onClick={()=>minusQuantity(newItem)}>-</button>
-                  <span className="quantity-text center">{newItem.quantity}</span>
-                  <button className="quantity-btn add-btn center" onClick={()=>addQuantity(newItem)}>+</button>
-                </li>
-              )
-            })}
-          </ul>
+  return (
+    <main id="cart">
+      <h2>Your Cart</h2>
+      <div className="cart--item-list-container">
+        <ul className="item-list cart--item-list">
+          <Inside cartItem={cartItem} setCartItem={setCartItem} />
+        </ul>
+      </div>
+      <div className="total-section">
+        <div>
+          <h3>Total</h3>
         </div>
-        <div className="total-section">
-          <div>
-            <h3>Total</h3> 
-          </div>
-          <div>
-            <span className="total-number">£{total()}</span>
-          </div>
+        <div>
+          <span className="total-number">£{total()}</span>
         </div>
-      </main>
-    )
+      </div>
+    </main>
+  );
 }
-export default Main
+export default Main;
