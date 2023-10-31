e8a10c3993f3398eed71958883a7afcf089406f7
Jack Miller <jdmiller1991@outlook.com>
2023-10-31 14:56:12 +0000

    Core done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './styles/index.css'
 
 import { useState } from 'react'
 import initialStoreItems from './store-items'
+import Header from './Components/Header'
+import Cart from './Components/Cart'
+import Footer from './Components/Footer'
 
 /*
  Here's what a store item should look like
@@ -60,67 +63,11 @@ export default function App() {
   
     return formattedMoney;
   }
-  
 
   return (
     <>
-      <header id="store">
-        <h1>Greengrocers</h1>
-        <ul className="item-list store--item-list">
-          {items.map((item) => (
-            <li key={item.id}>
-              <div className="store--item-icon">
-                <img src={`/assets/icons/${item.id}.svg`} alt={item.name} />
-              </div>
-              <button onClick={() => addToCart(item)}>Add to cart</button>
-              <p>{item.name.toUpperCase()}</p>
-              <p>${item.price.toFixed(2)}</p>
-            </li>
-          ))}
-
-        </ul>
-      </header>
-      <main id="cart">
-        <h2>Your Cart</h2>
-        <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
-            {cart.map((item) => (
-              <li key={item.id}>
-                <div className="cart--item-icon">
-                  <img src={`/assets/icons/${item.id}.svg`} alt={item.name} />
-                </div>
-                <p>{item.name.toUpperCase()}</p>
-                <button className="quantity-btn remove-btn center" onClick={() => removeFromCart(item)}>
-                  -
-                </button>
-                <span className="quantity-text center">{item.quantity}</span>
-                <button className="quantity-btn add-btn center" onClick={() => addToCart(item)}>+</button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="total-section">
-          <div>
-            <h3>Total</h3>
-          </div>
-          <div>
-            <span className="total-number">{cartPrice()}</span>
-          </div>
-        </div>
-      </main >
-      <div>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/icongeek26"
-          title="Icongeek26"
-        >
-          Icongeek26
-        </a>
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
+      <Header items={items} addToCart={addToCart} />
+      <Cart cart={cart} removeFromCart={removeFromCart} addToCart={addToCart} cartPrice={cartPrice} />
+      <Footer />
     </>
-  )
-}
+  )}
