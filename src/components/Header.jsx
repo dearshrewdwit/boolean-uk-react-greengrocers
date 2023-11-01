c413de4ad2edee8477d97ca747a124de8e4fa389
Satoki <satokiito@gmail.com>
2023-11-01 01:39:21 +0000

    extension 1 - basic sort function added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,23 +20,52 @@ function Header({store, addItem, cartItem, setCartItem}) {
 
     // EXTENSION 1
     const [filter, setFilter] = useState('none')
+    const [sort, setSort] = useState('none')
 
     const vegItems = filterByType.filter((item) => item.type === 'veg')
     const fruitItems = filterByType.filter((item) => item.type === 'fruit')
 
+    function alphabetical() {
+        const AToZ = filterByType.sort((a, b) => {
+            const nameA = a.name.toUpperCase()
+            const nameB = b.name.toUpperCase()
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+        })
+        return AToZ
+    }
+    
+    function reverseAlphabetical() {
+        return alphabetical().reverse()
+    }
+
     let storeItemList
     if (filter === 'fruit') storeItemList = fruitItems
     else if (filter === 'veg') storeItemList = vegItems
+    else if (sort === 'AToZ') storeItemList = alphabetical()
+    else if (sort === 'ZToA') storeItemList = reverseAlphabetical()
     else storeItemList = filterByType
 
     return (
         <header id="store">
             <h1>Greengrocers</h1>
-            <div className="filter-container">
-                <h2>Filter</h2>
-                <button onClick={() => setFilter('none')}>No Filter</button>
-                <button onClick={() => setFilter('fruit')}>Fruit</button>
-                <button onClick={() => setFilter('veg')}>Vegetables</button>
+            <div className="sort-filter-container">
+                <div className="filter-container">
+                    <h2>Filter</h2>
+                    <button onClick={() => setFilter('none')}>No Filter</button>
+                    <button onClick={() => setFilter('fruit')}>Fruit</button>
+                    <button onClick={() => setFilter('veg')}>Vegetables</button>
+                </div>
+                <div className="sort-container">
+                    <h2>Sort</h2>
+                    <button onClick={() => setSort('none')}>No Sort</button>
+                    <button onClick={() => setSort('AToZ')}>A-Z</button>
+                    <button onClick={() => setSort('ZToA')}>Z-A</button>
+                </div>
             </div>
             <ul className="item-list store--item-list">
                 {storeItemList.map((item) =>
