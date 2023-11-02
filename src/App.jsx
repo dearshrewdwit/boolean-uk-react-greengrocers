6fc7c11539d2050b2a0a56b1dff85f64d7b626e4
Papi <test@Tests-Air.fritz.box>
2023-11-02 13:48:09 +0100

    Papi

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './styles/reset.css'
 import './styles/index.css'
 
+import { useState } from 'react'
 import initialStoreItems from './store-items'
+import Header from './Header'
+import Cart from './Cart'
+import Footer from './Footer'
 
 /*
  Here's what a store item should look like
@@ -10,14 +14,54 @@ import initialStoreItems from './store-items'
  name: 'beetroot',
  price: 0.35
  }
-
  What should a cart item look like? 🤔
  */
-
 console.log(initialStoreItems)
 
 export default function App() {
-  // Setup state here...
+  //  creating state here...
+
+  const [items, setItems] = useState(initialStoreItems)
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    const updatedCart = [...cart];
+    const existingItemIndex = updatedCart.findIndex((cartItem) => cartItem.id === item.id);
+
+    if (existingItemIndex !== -1) {
+      updatedCart[existingItemIndex].quantity += 1;
+    } else {
+      updatedCart.push({ ...item, quantity: 1 });
+    }
+    setCart(updatedCart);
+  };
+
+  const removeFromCart = (item) => {
+    const updatedCart = [...cart];
+    const existingItemIndex = updatedCart.findIndex((cartItem) => cartItem.id === item.id);
+
+    if (existingItemIndex !== -1) {
+      const updatedItem = { ...updatedCart[existingItemIndex] };
+      if (updatedItem.quantity > 1) {
+        updatedItem.quantity -= 1;
+        updatedCart[existingItemIndex] = updatedItem;
+      } else {
+        updatedCart.splice(existingItemIndex, 1);
+      }
+
+      setCart(updatedCart);
+    }
+  };
+
+  function cartPrice() {
+    const money = cart.reduce((sum, item) => {
+      return sum + (item.price * item.quantity);
+    }, 0);
+
+    const formattedMoney = `£${money.toFixed(2)}`;
+
+    return formattedMoney;
+  }
 
   return (
     <>
@@ -56,6 +100,10 @@ export default function App() {
           www.flaticon.com
         </a>
       </div>
+      <Header items={items} addToCart={addToCart} />
+      <Cart cart={cart} removeFromCart={removeFromCart} addToCart={addToCart} cartPrice={cartPrice} />
+      <Footer />
     </>
   )
 }
+  
