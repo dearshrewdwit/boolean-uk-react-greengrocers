4d8f48e72509d32fdcf5057f7ee7dd54ff64b0d7
Chloe Zermatten <chloezermatten@gmail.com>
2023-11-17 13:52:03 +0000

    refactor into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,140 +1,26 @@
 import "./styles/reset.css";
 import "./styles/index.css";
+import { addItemToCart, calculator } from "../utilities/functions";
+
 
 import initialStoreItems from "./store-items";
 import { useState } from "react";
-
-/*
- Here's what a store item should look like
- {
- id: '001-beetroot',
- name: 'beetroot',
- price: 0.35
- }
-
- What should a cart item look like? 🤔
- */
-
+import Cart from "./components/Cart";
+import Store from "./components/Store";
 
 
 export default function App() {
-  // Setup state here...
-  const [items] = useState(initialStoreItems);
+  const initialStoreItemsWithQuantities = initialStoreItems.map(item => ({...item, quantity: 1}))
+
+  const [items, setItems] = useState(initialStoreItemsWithQuantities);
   const [itemsInCart, setItemsInCart] = useState([]);
   const [total, setTotal] = useState(0)
-  
-
-  // const itemsInCart = [
-  //    {
-  //       ...itemsInCart,
-  //        quantity : 1          
-  //     }    
-  // ]
-
-  const calculator = () => {const sum = itemsInCart.reduce(
-    (total, currentValue) => total + (currentValue.price * currentValue.quantity),
-    0,
-  );  
-  setTotal(sum);} 
-
-  
-  const addItemToCart = (event) => {
-    
-    const foundItem = itemsInCart.find(itemInTheList => itemInTheList.name === event.target.value)
-    const updatedCartItem = items.find(el =>  event.target.value === el.name)
-    
-
-      if (!foundItem) {
-
-        setItemsInCart([...itemsInCart, {...updatedCartItem, quantity: 1 }]);
-        
-        console.log(updatedCartItem)
-        //:)
-        calculator()
-      }else {
-        foundItem.quantity++;
-        setItemsInCart([...itemsInCart]);
-        console.log(itemsInCart)
-        calculator()
-      }   
-   }
-
-   const minusButton = (e) => {
-    if (e.quantity === 1){
-      const updatedItems = itemsInCart.filter(item => item !== e)
-      setItemsInCart(updatedItems)
-
-    }else{
-      e.quantity--
-      setItemsInCart([...itemsInCart])
-      console.log(itemsInCart)
-    }
-    calculator()
-   }
-
-   const plusButton = (e) => {
-
-    e.quantity++
-    setItemsInCart([...itemsInCart])
-    console.log(itemsInCart)
-    calculator()
-   }
-
-
-
-      
-
 
 
   return (
     <>
-      <header id="store">
-        <h1>Greengrocers</h1>
-        <ul className="item-list store--item-list">
-          {items.map((element, index) => (
-              <li key={index}>
-                <div className="store--item-icon">
-                  <img
-                    src={`../public/assets/icons/${element.id}.svg `}
-                    alt={element.name}
-                  />
-                </div>
-                <button 
-                  value={element.name}
-                  onClick={event => addItemToCart(event)}
-                >Add to cart</button>
-              </li>
-          ))}
-        </ul>
-      </header>
-      <main id="cart">
-        <h2>Your Cart</h2>
-        <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
-            {itemsInCart.map((e, index) => (
-              <li key={index}>
-                <img
-                  className="cart--item-icon"
-                  src={`../public/assets/icons/${e.id}.svg `}
-                  alt={e.name}
-                />
-                <p>{e.name}</p>
-                <button className="quantity-btn remove-btn center" onClick={() => minusButton(e)}>-</button>
-                <span className="quantity-text center">{e.quantity}</span>
-                <button className="quantity-btn add-btn center" onClick={() => plusButton(e)}>+</button>
-            </li>
-            ))}
-          </ul>
-        </div>
-        <div className="total-section">
-          <div>
-            <h3>Total</h3>
-          </div>
-          <div>
-            <span className="total-number">{`£ ${total.toFixed(2)}`}</span>
-          </div>
-        </div>
-      </main>
+      <Store itemsInStore={items} setItemsInStore={setItems} total={total} setTotal={setTotal} items={items} itemsInCart={itemsInCart} setItemsInCart={setItemsInCart}/>
+      <Cart itemsInCart={itemsInCart} setItemsInCart={setItemsInCart} total={total} setTotal={setTotal}/>
       <div>
         Icons made by
         <a
