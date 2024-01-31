b6070d9d92328e6a9d7b9a79e37751e775bfd075
Radio58 <rt58gm@gmail.com>
2024-01-31 14:38:24 +0000

    display store list

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,16 @@
+import StoreItem from "./StoreItem";
+import initialStoreItems from '../store-items'
+
 function Header({ items, setCart }) {
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {}
+          {initialStoreItems.map((e) => {
+            console.log(e)
+            return <StoreItem item={e} key={e.id}/>
+          })}
         </ul>
       </header>
     </>
---
+++ src/Components/StoreItem.jsx
@@ -0,0 +1,17 @@
+
+function StoreItem({ item }) {
+    
+    return (
+        <>
+        <li>
+            <div className="store--item-icon">
+                <img src={`../../public/assets/icons/${item.id}.svg`} alt={item.name}/>
+            </div>
+            <button>Add to cart</button>
+        </li>
+
+        </>
+  );
+}
+
+export default StoreItem;
