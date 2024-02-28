31bf7bff10533aa646fb5735b12c033f3f575763
pkekkonen <paulina.kekkonen@hotmail.com>
2024-02-28 15:57:10 +0100

    Completed core and filtering

--- src/store-items.js
+++ src/store-items.js
@@ -1,55 +1,64 @@
-
-const storeItems =  [
+const storeItems = [
   {
     id: "001-beetroot",
     name: "beetroot",
-    price: 0.35
+    price: 0.35,
+    type: "vegetable",
   },
   {
     id: "002-carrot",
     name: "carrot",
-    price: 0.35
+    price: 0.35,
+    type: "vegetable",
   },
   {
     id: "003-apple",
     name: "apple",
-    price: 0.35
+    price: 0.35,
+    type: "fruit",
   },
   {
     id: "004-apricot",
     name: "apricot",
-    price: 0.35
+    price: 0.35,
+    type: "fruit",
   },
   {
     id: "005-avocado",
     name: "avocado",
-    price: 0.35
+    price: 0.35,
+    type: "fruit",
   },
   {
     id: "006-bananas",
     name: "bananas",
-    price: 0.35
+    price: 0.35,
+    type: "fruit",
   },
   {
     id: "007-bell-pepper",
     name: "bell pepper",
-    price: 0.35
+    price: 0.35,
+    type: "fruit",
   },
   {
     id: "008-berry",
     name: "berry",
-    price: 0.35
+    price: 0.35,
+    type: "berry",
   },
   {
     id: "009-blueberry",
     name: "blueberry",
-    price: 0.35
+    price: 0.35,
+    type: "berry",
   },
   {
     id: "010-eggplant",
     name: "eggplant",
-    price: 0.35
-  }
-]
+    price: 0.35,
+    type: "vegetable",
+  },
+];
 
-export default storeItems
+export default storeItems;
