c1f772e8d46f14836c939500c22404a5095933d7
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-04 15:58:19 +0100

    Extension 2 - ProductView component, providing more information regarding clicked item

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import "./Header.css"
 import SortAndFilter from "./SortAndFilter/SortAndFilter.jsx"
+import ProductView from "./ProductView/ProductView.jsx"
 import PropTypes from 'prop-types'
 
 const Header = ({storeItems, addFunction}) => {
     const [filteredItems, setFilteredItems] = useState(storeItems)
+    const [detailView, setDetailView] = useState(undefined)
 
     const capitalizeString = (string) => {
         return (string.substring(0,1).toUpperCase() + string.substring(1,string.length))
@@ -14,12 +16,14 @@ const Header = ({storeItems, addFunction}) => {
         <header id="store">
         <h1>Greengrocers</h1>
         <SortAndFilter storeItems={storeItems} setFilteredItems={setFilteredItems}/>
+        {detailView && <ProductView product={detailView} setDetailView={setDetailView}/>}
         <ul className="item-list store--item-list">
             {filteredItems.map((entry, index) => 
                 <li key={index}>
                     <img 
                         className="store--item-icon" 
                         src={"/assets/icons/"+entry.id+".svg"} 
+                        onClick={() => setDetailView(entry)}
                     />
                     <p>{capitalizeString(entry.name)}</p>
                     <button onClick={() => addFunction(entry.id)}>add to cart</button>
