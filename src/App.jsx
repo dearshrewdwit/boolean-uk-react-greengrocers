b5b20b2d1688b79e29e75129cfc6c6fc68d973d7
Nathan King <nathanking3590@gmail.com>
2024-04-28 09:19:50 +0100

    remove unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,18 @@ import './styles/index.css'
 
 import initialStoreItems from './store-items'
 
-/*
- Here's what a store item should look like
- {
- id: '001-beetroot',
- name: 'beetroot',
- price: 0.35
- }
-
- What should a cart item look like? 🤔
- */
-
-console.log(initialStoreItems)
-
 export default function App() {
-  // Setup state here...
-
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Write some code here... */}
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Write some code here... */}
           </ul>
         </div>
         <div className="total-section">
