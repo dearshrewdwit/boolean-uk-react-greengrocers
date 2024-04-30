b9eba7f16ba6c1cf700ffae1e1bd1ea76171e26f
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-04-30 15:32:44 +0100

    core

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,18 @@
 import './styles/reset.css'
 import './styles/index.css'
 
-import initialStoreItems from './store-items'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import { useState } from 'react'
 
 export default function App() {
+  const [basket, setBasket] = useState([])
   return (
     <>
-      <header id="store">
-        <h1>Greengrocers</h1>
-        <ul className="item-list store--item-list">
-        </ul>
-      </header>
-      <main id="cart">
-        <h2>Your Cart</h2>
-        <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
-          </ul>
-        </div>
-        <div className="total-section">
-          <div>
-            <h3>Total</h3>
-          </div>
-          <div>
-            <span className="total-number">£0.00</span>
-          </div>
-        </div>
-      </main>
-      <div>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/icongeek26"
-          title="Icongeek26"
-        >
-          Icongeek26
-        </a>
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
+      <Header setBasket={setBasket} basket={basket}></Header>
+      <MainContent basket={basket} setBasket={setBasket}></MainContent>
+      <Footer></Footer>
     </>
   )
 }
